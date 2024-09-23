787591feb45bed15e23413f81563bb51f5e41fb0
João Brasil <joao.brasil@gazin.com.br>
2024-09-23 18:47:29 -0300

    first commit

--- api/activitiesopen.ts
+++ api/activitiesopen.ts
@@ -5,7 +5,15 @@ import Activity from '../model/activity';
 
 
 export default async function handler(req: Request, res: Response) {
-  
+   // Adicionar cabeçalhos CORS
+ res.setHeader('Access-Control-Allow-Origin', '*'); // Permitir todas as origens
+ res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS'); // Métodos permitidos
+ res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Cabeçalhos permitidos
+
+ // Verifica se é uma requisição OPTIONS (pré-flight request para CORS)
+ if (req.method === 'OPTIONS') {
+   return res.status(200).end(); // Responder imediatamente a requisições OPTIONS
+ }
   await connectDB();
 
   try {
@@ -19,4 +27,6 @@ export default async function handler(req: Request, res: Response) {
   } catch (error) {
     return res.status(500).json({ error: 'Erro ao buscar atividades em aberto.' });
   }
+  
 }
+
