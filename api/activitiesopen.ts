229bb758fad03579c88bda0c91cd3a0e94957bc2
João Brasil <joao.brasil@gazin.com.br>
2024-09-23 18:40:23 -0300

    first commit

--- api/activitiesopen.ts
+++ api/activitiesopen.ts
@@ -3,7 +3,17 @@ import { Request, Response } from 'express';
 import connectDB from '../config/database';
 import Activity from '../model/activity';
 
+
 export default async function handler(req: Request, res: Response) {
+     // Adicionar cabeçalhos CORS
+     res.setHeader('Access-Control-Allow-Origin', '*'); // Permitir todas as origens
+     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Métodos permitidos
+     res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Cabeçalhos permitidos
+   
+     // Verifica se é uma requisição OPTIONS (pré-flight request para CORS)
+     if (req.method === 'OPTIONS') {
+       return res.status(200).end(); // Responder imediatamente a requisições OPTIONS
+     }
   await connectDB();
 
   try {
